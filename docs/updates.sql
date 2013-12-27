940cda6dde82ae0a657c4aa463012e4e41ce0942
esemi <spam@esemi.ru>
2013-12-27 14:05:38 +0400

    Complete trigger and db updates for new playr changes table

--- docs/updates.sql
+++ docs/updates.sql
@@ -36,19 +36,25 @@ DROP TABLE players_trans_colony;
 DROP TABLE players_trans_dom;
 
 
-
-
-
 CREATE TABLE IF NOT EXISTS `players_changes` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `id_player` int(10) unsigned NOT NULL,
   `type` enum('gate_open','gate_close') NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `id_player` (`id_player`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  KEY `id_player` (`id_player`),
+  KEY `date` (`date`),
+  KEY `type` (`type`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-ALTER TABLE `players_changes` ADD FOREIGN KEY ( `id_player` ) REFERENCES `dseye_new`.`players` (
-`id`
-) ON DELETE CASCADE ON UPDATE CASCADE ;
+ALTER TABLE `players_changes`
+  ADD CONSTRAINT `players_changes_ibfk_1` FOREIGN KEY (`id_player`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+INSERT INTO `players_changes` ( id_player, `type` , `date` )
+SELECT `id_player` , IF( `new_gate` =1, 'gate_open', 'gate_close' ) AS `type` , `date`
+FROM `players_trans_gate`;
+
+DROP TABLE players_trans_gate;
+
+ALTER TABLE `players_changes` CHANGE `type` `type` ENUM( 'gate_open', 'gate_close', 'premium_enable', 'premium_disable', 'ban', 'unban',
+'newbee_enable', 'newbee_disable', 'shield_enable', 'shield_disable' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
