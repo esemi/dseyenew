15d613374f2744fd6454e3f9b9cc0760dadccba3
esemi <spam@esemi.ru>
2014-01-08 14:29:54 +0400

    Replace highcharts to highstock

--- docs/combine.sh
+++ docs/combine.sh
@@ -2,7 +2,7 @@
 
 LIBS_DIR="../www/js/libs/";
 RESULT_FILENAME="combine.js";
-FILES=('jQ.min.js' 'jQui.min.js' 'jquery.dimensions.min.js' 'jquery.tooltip.min.js' 'highcharts.js');
+FILES=('jQ.min.js' 'jQui.min.js' 'jquery.dimensions.min.js' 'jquery.tooltip.min.js' 'highstock.js');
 
 touch $LIBS_DIR/$RESULT_FILENAME;
 cat /dev/null > $LIBS_DIR/$RESULT_FILENAME;
@@ -10,6 +10,7 @@ cat /dev/null > $LIBS_DIR/$RESULT_FILENAME;
 for f in "${FILES[@]}"
 do
 	echo $f;
+	echo "/*********** start delimeter **********/" >> $LIBS_DIR/$RESULT_FILENAME;
 	cat $LIBS_DIR/$f >> $LIBS_DIR/$RESULT_FILENAME;
-	echo "/*********** delimeter **********/" >> $LIBS_DIR/$RESULT_FILENAME;
+	echo "/*********** end delimeter **********/" >> $LIBS_DIR/$RESULT_FILENAME;
 done;
